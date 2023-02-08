9913251573d087e28f08245ebd9aaef383f4b604
Ron Campbell <roncampbell@users.noreply.github.com>
2023-02-08 11:26:59 -0800

    Update IRS reference.R

--- IRS reference.R
+++ IRS reference.R
@@ -5,6 +5,7 @@
 # this script imports, unzips and cleans tax-exempt organization files from IRS
 # the files range in size from 90 MB to 236 MB
 
+# set working directory, then load packages (must be installed first)
 setwd()
 library(tidyverse)
 library(httr)
@@ -29,3 +30,36 @@ colnames(Charities)[3] <- 'City'
 colnames(Charities)[4] <- 'State'
 colnames(Charities)[5] <- 'Country'
 colnames(Charities)[6] <- 'Type'
+
+# import list of nonprofits that lost their tax-exempt status
+temp2 <- tempfile()
+download.file("https://apps.irs.gov/pub/epostcard/data-download-revocation.zip", 
+              temp2, mode = "wb")
+unzip(temp2, "data-download-revocation.txt")
+unlink(temp2)
+Revoked <- read_delim("data-download-revocation.txt", delim = "|", skip = 2,
+                      col_names = FALSE)
+
+# View Revoked data frame - 12 unnamed columns
+View(Revoked)
+
+# add names to columns in Revoked data frame
+colnames(Revoked)[1] <- 'EIN'
+colnames(Revoked)[2] <- 'Organization'
+colnames(Revoked)[3] <- 'OrgSuffix'
+colnames(Revoked)[4] <- 'Address'
+colnames(Revoked)[5] <- 'City'
+colnames(Revoked)[6] <- 'State'
+colnames(Revoked)[7] <- 'ZIP'
+colnames(Revoked)[8] <- 'Country'
+colnames(Revoked)[9] <- 'Exempt'
+colnames(Revoked)[10] <- 'Date1'
+colnames(Revoked)[11] <- 'Date2'
+colnames(Revoked)[12] <- 'Date3'
+
+# convert date fields in Revoked from character to date format
+Revoked <- Revoked %>% 
+  mutate(Date1 = dmy(Date1),
+         Date2 = dmy(Date2),
+         Date3 = dmy(Date3))
+
